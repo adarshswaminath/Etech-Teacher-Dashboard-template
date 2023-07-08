59cfd38d713724ac5f69de6ed7dbdb2b44a92476
Adarsh <adarshswaminath7@gmail.com>
2023-07-09 00:29:09 +0530

    scheme option selecting added

---
+++ src/Components/Schedule/Box.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+function Box() {
+  const [selectedButton, setSelectedButton] = useState('');
+
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName);
+  };
+
+  return (
+    <div>
+      {/* 4 buttons */}
+      <div className="text-center grid gap-2 grid-cols-2 lg:grid-cols-4">
+        <div
+          className={`px-2 py-4 rounded-lg ${
+            selectedButton === 'All' ? 'bg-[#eb6270] text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleButtonClick('All')}
+        >
+          All Lessons
+        </div>
+        <div
+          className={`px-2 py-4 rounded-lg ${
+            selectedButton === 'OneByOne' ? 'bg-[#eb6270] text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleButtonClick('OneByOne')}
+        >
+          One By One
+        </div>
+        <div
+          className={`px-2 py-4 rounded-lg ${
+            selectedButton === 'Webinar' ? 'bg-[#eb6270] text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleButtonClick('Webinar')}
+        >
+          Webinar
+        </div>
+        <div
+          className={`px-2 py-4 rounded-lg ${
+            selectedButton === 'VideoScheduling' ? 'bg-[#eb6270] text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleButtonClick('VideoScheduling')}
+        >
+          Video Scheduling
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Box;
--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import Search from '../Search'
+import {BsFilterCircleFill} from "react-icons/bs"
+import Box from './Box'
 
 function Schedule() {
   return (
-    <div>
-        <h1 className='text-4xl text-center text-black'>Schdule On working</h1>
+    <div className="p-2 md:p-4 lg:p-6 xl:p-8">
+        <div className="grid lg:flex gap-4">
+          {/* first section */}
+          <div className='w-full lg:w-2/3'>
+            <div className="grid lg:flex justify-between">
+              <div className="grid  mb-2">
+                <h3 className='text-3xl text-violet-600 font-semibold'>My Schedule</h3>
+                <p className='text-gray-500'>Education Teacher Dashboard</p>
+              </div>
+              <Search/>
+            </div>
+            {/* trend course */}
+            <div className="flex justify-between items-center mt-3 mb-3">
+              <div className='flex items-center gap-4'>
+                <div className='border-4 hover:border-b-[#ecb8c2] duration-300'>
+                  <h3 className='text-3xl font-semibold text-violet-600'>Top Trend Course</h3>
+                </div>
+                <div className='border-4 hover:border-b-[#ecb8c2]'>
+                  <h3 className='hidden lg:flex text-violet-600 font-semibold'>New Course</h3>
+                </div>
+              </div>
+              <div className='flex'>
+                <button className='btn'>
+                  Filter
+                  <BsFilterCircleFill/>
+                </button>
+              </div>
+            </div>
+            <Box/>
+          </div>
+
+          {/* second box */}
+          <div className='w-full lg:w-1/3 bg-white'>2</div>
+          
+        </div>
     </div>
   )
 }
